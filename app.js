abbaca66dba6bd9fc47389aee3d517b9a5b539f6
TaniaPuc <61524542+Tania-Puc@users.noreply.github.com>
2021-09-29 10:51:48 -0500

    Instalación y configuracion
    Se instalaron los modulos correspondientes asi como se ha iniciado la configuración del entorno.

---
+++ app.js
@@ -0,0 +1,20 @@
+const { json } = require('express');
+const express=require('express');
+
+
+
+const app=express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended:false}));
+
+app.use(express(json));
+
+app.use('/', require('./router'));
+
+app.set('port', process.env.PORT || 5000)
+
+app.listen(app.get('port'),()=>{
+    console.log('Server corriendo en ', app.get('port'))
+})
